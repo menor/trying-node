e191e8cfdd3034dde5c101ed4368d3aa5f6ef319
menor <josemenorgmail.com>
2014-04-14 10:49:17 -0700

    Create a basic http server.

---
+++ app.js
@@ -0,0 +1,24 @@
+/*jslint node: true */
+'use strict';
+
+var http = require( 'http' );
+
+var mappings = {
+    g: 'http://www.google.com'
+};
+
+var server = http.createServer( function ( req, res ) {
+    var alias = req.url.substring( 1 );
+
+    if( !mappings[alias] ) {
+        res.writeHead(404);
+        return res.end();
+    }
+
+    res.writeHead( 302, {
+        location: mappings[alias]
+    });
+    res.end();
+});
+
+server.listen( 3000 );
